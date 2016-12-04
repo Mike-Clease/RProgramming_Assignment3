b1b1237e95911b6a144caf81a8920f899cf6b8e0
MikeClease <clease.m@gmail.com>
2016-12-04 10:32:30 +0000

    Just getting all the placeholders uploaded

---
+++ Best.R
@@ -0,0 +1,10 @@
+## This function returns the best performing hospital in a named
+## state for a named morbidity (heart attack, heart failure or pneumonia)
+
+best <- function(state,outcome){
+
+data <- read.csv("outcome-of-care-measures.csv", colClasses = "character") #read outcome data to 'data'
+
+
+
+}
